71527a6954c8b06193f4f4c0a3ce7451f3014da3
Richard Sipes <rksipes@gmail.com>
2016-10-28 12:56:29 -0700

    new requirements.txt

--- runserver.py
+++ runserver.py
@@ -1,5 +1,8 @@
+import os
 from catalog import app
 
-app.debug = True
-app.secret_key = 'super-secret-key'
-app.run(host='0.0.0.0', port=5000)
+if __name__ == 'main':
+    port = int(os.environ.get("PORT", 33507))
+    app.debug = True
+    app.secret_key = 'super-secret-key'
+    app.run(host='0.0.0.0', port=5000)
