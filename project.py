270374b9d5b118f09ccd2d296bf32a0e1d252c36
Richard Sipes <rksipes@gmail.com>
2016-12-12 18:47:45 -0800

    final submission for udacity

--- project.py
+++ project.py
@@ -5,7 +5,8 @@ from catalog import app
 # app.secret_key = 'super-secret-key'
 # app.run(host='0.0.0.0', port=5000)
 
-# For Heroku Deployment
+# For Local Deployment
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = 'super-secret-key'
+    app.run(host='0.0.0.0', port=5000)
