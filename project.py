a72e508fe58f5cae113ef71512897df64fe6fabe
Richard Sipes <rksipes@gmail.com>
2016-12-12 18:25:52 -0800

    changes for heroku deployment

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 from catalog import app
 
-# if __name__ == 'main':
-app.debug = True
-app.secret_key = 'super-secret-key'
-# app.run(host='0.0.0.0', port=5000)
+if __name__ == 'main':
+    app.debug = True
+    app.secret_key = 'super-secret-key'
+    app.run()
