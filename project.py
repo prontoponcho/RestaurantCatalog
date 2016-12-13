493212f9986027026d3de7adaff1d91aae6b25ff
Richard Sipes <rksipes@gmail.com>
2016-12-12 19:02:45 -0800

    heroku deployment

--- project.py
+++ project.py
@@ -1,12 +1,12 @@
 from catalog import app
 
 # if __name__ == 'main':
-# app.debug = True
-# app.secret_key = 'super-secret-key'
+app.debug = True
+app.secret_key = 'super-secret-key'
 # app.run(host='0.0.0.0', port=5000)
 
 # For Local Deployment
-if __name__ == "__main__":
-    app.debug = True
-    app.secret_key = 'super-secret-key'
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     app.debug = True
+#     app.secret_key = 'super-secret-key'
+#     app.run(host='0.0.0.0', port=5000)
