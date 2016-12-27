c8499ae4204ab548d72a1dd20d39df1b98e53423
Richard Sipes <rksipes@gmail.com>
2016-12-26 18:28:55 -0800

    branch for third party login

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     app.debug = True
     app.secret_key = 'super-secret-key'
     app.run(host='0.0.0.0', port=5000)
+# For Heroku Deployment
 else:
     app.debug = False
     app.secret_key = 'super-secret-key'
