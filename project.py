ee1add33719dbb30b801ae1e368784285f5a498a
Richard Sipes <rksipes@gmail.com>
2016-12-26 18:43:26 -0800

    test changes

--- project.py
+++ project.py
@@ -1,10 +1,11 @@
 from catalog import app
 
-# For Local Deployment
+# For local deployment
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = 'super-secret-key'
     app.run(host='0.0.0.0', port=5000)
+# For live deployment
 else:
     app.debug = False
     app.secret_key = 'super-secret-key'
